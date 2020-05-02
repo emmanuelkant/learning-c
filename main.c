1e2908d574e7c77ccf031e7c7a29e90e88ed16cf
Emmanuel Kant Duarte <emmanuelkant.duarte@gmail.com>
2020-05-02 15:34:48 -0300

    No Message

---
+++ main.c
@@ -0,0 +1,13 @@
+/* 
+Author: Emmanuel Kant Duarte
+Date: 05/02/2020
+*/
+#include <stdio.h>
+
+int main() {
+
+  printf("Oh yeah, I'm learning C program language\n");
+  printf("This is my program");
+
+  return 0;
+}
